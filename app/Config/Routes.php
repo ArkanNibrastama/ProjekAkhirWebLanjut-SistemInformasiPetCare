411c06200a9ff32745b50b050eb47148ebea688a
ArkanNibrastama <arkan6040nibrastama@gmail.com>
2023-11-06 16:54:45 +0700

    add service and product page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,5 @@ $routes->get('/dashboard', 'Admin::index');
 $routes->get('/category', 'Admin::listjproduk');
 $routes->get('/produk', 'Admin::listproduk');
 $routes->get('/user','UserController::index');
+$routes->get('/user/products','UserController::products');
+$routes->get('/user/services','UserController::services');
